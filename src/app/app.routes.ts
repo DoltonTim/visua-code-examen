b140e1bca443ab72b5b2c9524c48adf8605c982e
Dolton Tom Meza Arevalo <doltontomsistemas@gmail.com>
2024-10-29 19:09:56 -0600

    acptar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { EscuelaComponent } from './escuela/escuela.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+{
+  path:'',
+  component:HomeComponent,
+  title:'home'
+
+},
+{
+  path:'escuela',
+  component:EscuelaComponent,
+  title:'escuela'
+
+},
+
+{
+
+  path:'**',
+  redirectTo:'',
+  pathMatch:'full'
+}
+
+];
